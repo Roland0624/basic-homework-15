576e5294135b53c3b5f3c13dfc98040bba658113
Roland0624 <48576613+Roland0624@users.noreply.github.com>
2019-04-11 19:21:08 +0800

    Add files via upload

---
+++ HW15.cpp
@@ -0,0 +1,21 @@
+#include<stdio.h> 
+#include<stdlib.h>
+
+
+int main()
+{ 
+    int hour,minute;
+	printf("請輸入時間: ");
+	scanf("%d%d",&hour,&minute); 
+	int hourtominute=hour*60;
+	
+	if(hourtominute+minute>=450&&hourtominute+minute<=1020)
+	{
+	printf("現在是上課時間\n");
+	}
+	else
+	{
+	printf("現在是下課時間\n");
+    }
+	
+}
